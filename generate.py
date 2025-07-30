242627d5f4ca2fb0f28a8e88fefce13b88ce5b47
Lachlan Lancaster <lachlanlancaster@gmail.com>
2025-07-30 17:10:03 -0400

    new generation methods

--- generate.py
+++ generate.py
@@ -63,6 +63,179 @@ def init_velocity_field_proj(sigma, kspec, kmin, kmax, N, seed=42):
     return (vx, vy, vz)
 
 
+def make_hermitian(arr, N):
+    # guarantees that the fourier transform is real
+    # first make sure maximum and zero frequency phases are zero
+    a = np.copy(arr)
+    a[0,0,0] = 0
+    # set nyquist frequncies to zero
+    a[N//2,:,:] = 0
+    a[:,N//2,:] = 0
+    a[:,:,N//2] = 0
+    # deal with zeros in two places
+    for i in range(1,N//2):
+        a[i,0,0] = np.conjugate(a[-i,0,0])
+        a[0,i,0] = np.conjugate(a[0,-i,0])
+        a[0,0,i] = np.conjugate(a[0,0,-i])
+    # deal with zero in one place
+    for i in range(1,N//2):
+        for j in range(1,N//2):
+            a[ i, j, 0] = np.conjugate(a[-i,-j, 0])
+            a[-i, j, 0] = np.conjugate(a[ i,-j, 0])
+            a[ i, 0, j] = np.conjugate(a[-i, 0,-j])
+            a[-i, 0, j] = np.conjugate(a[ i, 0,-j])
+            a[ 0, i, j] = np.conjugate(a[ 0,-i,-j])
+            a[ 0,-i, j] = np.conjugate(a[ 0, i,-j])
+    # deal with the bulk of the grid
+    for i in range(1,N//2):
+        for j in range(1,N//2):
+            for k in range(1,N//2):
+                a[ i, j, k] = np.conjugate(a[-i,-j,-k])
+                a[-i, j, k] = np.conjugate(a[ i,-j,-k])
+                a[ i,-j, k] = np.conjugate(a[-i, j,-k])
+                a[ i, j,-k] = np.conjugate(a[-i,-j, k])
+    return a
+
+
+def init_vec_potential(kspec, kmin, kmax, N, seed=42):
+    """
+    Initializes a 3D, periodic gaussian random field with a given power spectrum. Used to
+    initialize a vector potential that can then be used in real or fourier space to
+    create a divergence free velocity field
+
+    Parameters
+    ----------
+    kspec : float
+        The spectral index of the power spectrum
+    kmin : float
+        The minimum wavenumber for the power spectrum
+    kmax : float
+        The maximum wavenumber for the power spectrum
+    N : int
+        The size of the grid (N x N x N)
+    seed: int, optional
+        Random seed (default 42)
+    """
+    kmin *= 2 * np.pi
+    kmax *= 2 * np.pi
+    k = np.fft.fftfreq(N, d=1.0 / N) * 2 * np.pi
+    KS = np.meshgrid(k, k, k, indexing="ij")
+    (KX, KY, KZ) = KS
+    KS = np.array(KS)
+    K = np.sqrt(KX**2 + KY**2 + KZ**2)
+    mask = K > 0
+    mask1 = K < kmin
+    mask2 = K > kmax
+
+    AK = []
+    for i in range(3):
+        np.random.seed(seed + i)
+        ak = np.zeros((N, N, N), dtype=complex)
+        phase = np.exp(1.j*2*np.pi*np.random.normal(size=(N, N, N)))
+        sigma_norm = (K**2 + 0.001**2) ** (-0.5 * (kspec+1))
+        norms = np.random.rayleigh(sigma_norm)
+        ak[mask] = norms[mask]* phase[mask]
+        ak = make_hermitian(ak, N)
+        ak[mask2] *= np.exp(-1 * (K[mask2] / kmax) ** 2) * np.e
+        AK.append(ak)
+    AK = np.array(AK)
+    (ax, ay, az) = [np.fft.ifftn(ak).real for ak in AK]
+
+    return (ax, ay, az)
+
+
+def init_velocity_field_kcurl(sigma, kspec, kmin, kmax, N, seed=42):
+    """
+    Initializes a 3D, periodic gaussian random field with a given power spectrum
+
+    Parameters
+    ----------
+    kspec : float
+        The spectral index of the power spectrum
+    sigma : float
+        The standard deviation of the velocity field
+    kmin : float
+        The minimum wavenumber for the power spectrum
+    kmax : float
+        The maximum wavenumber for the power spectrum
+    N : int
+        The size of the grid (N x N x N)
+    seed: int, optional
+        Random seed (default 42)
+    """
+    k = np.fft.fftfreq(N, d=1.0 / N) * 2 * np.pi
+    KS = np.meshgrid(k, k, k, indexing="ij")
+    (KX, KY, KZ) = KS
+    KS = np.array(KS)
+    K = np.sqrt(KX**2 + KY**2 + KZ**2)
+
+    (ax, ay, az) = init_vec_potential(kspec, kmin, kmax, N, seed=seed)
+    A = np.array([ax, ay, az])
+    AK = np.array([np.fft.fftn(a) for a in A])
+    VK = np.zeros_like(AK, dtype=complex)
+
+    VK[0] = 1.j*(KS[1]*AK[2] - KS[2]*AK[1])
+    VK[1] = 1.j*(KS[2]*AK[0] - KS[0]*AK[2])
+    VK[2] = 1.j*(KS[0]*AK[1] - KS[1]*AK[0])
+
+    (vx, vy, vz) = [np.fft.ifftn(vk).real for vk in VK]
+    sigma_res = np.sqrt(np.std(vx)** 2 + np.std(vy)** 2 + np.std(vz)** 2)
+
+    DIVVK = np.sum(1.j*KS * VK, axis=0)
+    divv = (sigma/sigma_res)*np.fft.ifftn(DIVVK).real
+
+    vx *= sigma / sigma_res
+    vy *= sigma / sigma_res
+    vz *= sigma / sigma_res
+    return (vx, vy, vz)
+
+
+def init_velocity_field_curl(sigma, kspec, kmin, kmax, N, seed=42):
+    """
+    Initializes a 3D, periodic gaussian random field with a given power spectrum
+
+    Parameters
+    ----------
+    kspec : float
+        The spectral index of the power spectrum
+    sigma : float
+        The standard deviation of the velocity field
+    kmin : float
+        The minimum wavenumber for the power spectrum
+    kmax : float
+        The maximum wavenumber for the power spectrum
+    N : int
+        The size of the grid (N x N x N)
+    seed: int, optional
+        Random seed (default 42)
+    """
+    k = np.fft.fftfreq(N, d=1.0 / N) * 2 * np.pi
+    KS = np.meshgrid(k, k, k, indexing="ij")
+    (KX, KY, KZ) = KS
+    KS = np.array(KS)
+    K = np.sqrt(KX**2 + KY**2 + KZ**2)
+
+    (ax, ay, az) = init_vec_potential(kspec, kmin, kmax, N, seed=seed)
+
+    # take the curl of the vector potential
+    vx  = np.roll(az, -1, axis=1) - np.roll(az, 1, axis=1)
+    vx -= np.roll(ay, -1, axis=2) - np.roll(ay, 1, axis=2)
+    vy  = np.roll(ax, -1, axis=2) - np.roll(ax, 1, axis=2)
+    vy -= np.roll(az, -1, axis=0) - np.roll(az, 1, axis=0)
+    vz  = np.roll(ay, -1, axis=0) - np.roll(ay, 1, axis=0)
+    vz -= np.roll(ax, -1, axis=1) - np.roll(ax, 1, axis=1)
+    sigma_res = np.sqrt(np.std(vx)** 2 + np.std(vy)** 2 + np.std(vz)** 2)
+
+    divv  = np.roll(vx, -1, axis = 0) -  np.roll(vx, 1, axis = 0)
+    divv += np.roll(vy, -1, axis = 1) -  np.roll(vy, 1, axis = 1)
+    divv += np.roll(vz, -1, axis = 2) -  np.roll(vz, 1, axis = 2)
+
+    vx *= sigma / sigma_res
+    vy *= sigma / sigma_res
+    vz *= sigma / sigma_res
+    return (vx, vy, vz)
+
+
 def cell_center_coordinates(N: int = 128) -> tuple:
     """
     Returns a tuple of 3 arrays containing the Cartesian coordinate functions at the cell centers in the unit cube.
